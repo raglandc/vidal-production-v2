3975dd471385083e3610ef09ec0e822d1c19cc34
Chris Ragland <Chrisragland97@gmail.com>
2022-07-19 11:22:04 -0400

    [action] first article edit

--- src/routes/blog/_post.svelte
+++ src/routes/blog/_post.svelte
@@ -13,6 +13,7 @@
 </script>
 
 
+
     <header>
         <h1>{title}</h1>
         <div class="meta-data">
@@ -21,7 +22,9 @@
         </div>
     </header>
     <article>
-        <slot/>
+        <div class="article-body">
+            <slot/>
+        </div>
     </article>
 
 
@@ -30,21 +33,30 @@
     header {
         display: flex;
         flex-direction: column;
+        background-color: var(--background-color);
         align-items: center;
-        grid-column: 2 / -2;
+        grid-column: 1 / -1;
+        padding: 0 5%;
     }
     
     h1 {
         font-size: 4rem;
         color: var(--text-primary)
     }
-
+    
     article {
+        background-color: var(--text-primary);
         color: #111;
-        background: var(--text-primary);
         grid-column: 1 / -1;
-        padding: 5% 5% 0 ;
-        clip-path: polygon(0 8%, 100% 0, 100% 100%, 0 100%);
+        padding: 10% 0 ;
+        display: grid;
+        grid-template-columns: 5% repeat(4, 1fr) 5%;
+        margin: 0 0 13%;
+        clip-path: polygon(0 7%, 100% 0, 100% 100%, 0 100%);
+    }
+
+    .article-body{
+        grid-column: 2 / -2;
     }
 
     .meta-data {
@@ -54,5 +66,15 @@
         justify-content: space-between;
         align-items: center;
         color: var(--text-secondary);
+    }
+
+
+    @media only screen and (min-width: 700px){
+        article {
+            grid-template-columns: 10% repeat(12, 1fr) 10%;
+        }
+        .article-body {
+            grid-column: 3 / -3;
+        }
     }
 </style>
---
+++ src/routes/blog/how-to-add-glb-and-gltf-files-to-svelte-cubed-scene.md
@@ -0,0 +1,22 @@
+---
+title: 'How to add .glb/.gltf file to your Svelte Cubed scene'
+date: '07-18-2022'
+description: 'Learn how to add your .glb or .gltf files to your Svelte Cubed scene.'
+readTime: 10
+---
+
+<!-- <img src="https://images.unsplash.com/photo-1657641898365-48ae7d64e676?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="This is an image of a toy 3d robot model" width="100%" > -->
+
+# What is Svelte Cubed
+
+Svelte Cubed was created by Rich Harris, who is also the creator of Svelte.
+
+If you are familiar with Three.js, I am assuming you are if you are reading this article, then you are aware that there is a lot of boiler plate to writing just **one** scene.
+
+```js
+const thisIsJS: boolean;
+
+const arrowFunction = () => {
+	return true;
+};
+```
