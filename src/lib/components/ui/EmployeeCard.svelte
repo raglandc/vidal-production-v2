b992cc34aa1bffca476c5e55edd4095c383c4aa4
Chris Ragland <Chrisragland97@gmail.com>
2022-07-21 05:17:26 -0400

    [action] styles for employee card and section four of the homepage

--- src/lib/components/ui/EmployeeCard.svelte
+++ src/lib/components/ui/EmployeeCard.svelte
@@ -2,12 +2,15 @@
 export let url: string;
 export let description: string;
 export let name: string;
+export let role: string;
 </script>
 
 
 
 <div class="card">
     <div class="employee-picture" style="background-image: url({url});"/>
+    <h3>{name}</h3>
+    <p>{role}</p>
     <p class="description">{description}</p>
 </div>  
 <style>
@@ -15,11 +18,11 @@ export let name: string;
         display: flex;
         flex-direction: column;
         align-items: center;
-        height: 40rem;
+        height: 45rem;
     }
     .employee-picture {
-        width: 200px;
-        height: 200px;
+        width: 19.5rem;
+        height: 19.5rem;
         border-radius: 50% 50%; 
         background-size: cover;
         background-position: center;
@@ -28,7 +31,7 @@ export let name: string;
     .description {
         text-align: center;
         color: var(--text-secondary);
-        width: 60%;
+        width: 80%;
         line-height: 2;
     }
 </style>
--- src/lib/sections/HomePageSectionFour.svelte
+++ src/lib/sections/HomePageSectionFour.svelte
@@ -3,10 +3,10 @@
     import EmployeeCard from "$lib/components/ui/EmployeeCard.svelte";
 </script>
 
+<h1>Meet the team</h1>
 <section>
-    <h1>Meet the team</h1>
     <span class="card-one">
-        <EmployeeCard name="Chris" description="Chris founded Vidal with the intention of building beautiful websites that demonstrate the beauty behind simplicity" url="../../static/presidentVidal.jpg" />
+        <EmployeeCard role="President" name="Chris" description="Chris founded Vidal with the intention of building beautiful websites that demonstrate the beauty behind simplicity" url="../../static/presidentVidal.jpg" />
     </span>
 </section>
 
@@ -21,7 +21,7 @@
 
     h1 {
         font-size: 4rem;
-        margin: 15% 0;
+        margin: 10% 0;
         color: var(--text-primary);
         grid-column: -1 / 1;
         text-align: center;
@@ -31,4 +31,15 @@
         grid-column: 2 / -2;
     }
 
+
+    @media only screen and (min-width: 1600px) {
+        section {
+            grid-template-columns: 10% repeat(12, 1fr) 10%;
+            grid-gap: 2rem;
+        }
+
+        .card-one {
+            grid-column: 2 / 6;
+        }
+    }
 </style>
