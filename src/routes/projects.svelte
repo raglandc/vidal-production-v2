0da50886275f4236f3e45338140020d96533683c
Chris Ragland <Chrisragland97@gmail.com>
2022-07-30 13:44:37 -0400

    [action] create a function to handle animated scrolling effect

---
+++ src/routes/projects.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+import { onMount } from "svelte";
+
+
+let scrollPostion: number;
+let video: HTMLVideoElement;
+let videoLength:number;
+
+onMount(() => {
+    function scrollVideo() {
+        scrollPostion = document.documentElement.scrollTop;
+        videoLength = video.duration;
+        video.currentTime = (scrollPostion / (document.documentElement.scrollHeight - window.scrollY)) * videoLength;
+        console.log(video.currentTime);
+    }
+
+    window.addEventListener("scroll", () => {
+        scrollVideo();
+    })
+
+})
+
+
+
+</script>
+
+<section style="height:{video ? videoLength * 10 : "100"}vh;">
+    <h1>Projects</h1>
+    <video bind:this={video} src="../../static/blender.mp4">
+    <track kind="captions"/>
+    </video>
+</section>
+
+
+
+
+
+<style>
+    section {
+        position: relative;
+        grid-column: 1 / -1;
+    }
+
+    h1 {
+        margin: 0 10%;
+        color: var(--text-primary);
+    }
+
+    video {
+        position: fixed;
+        top: 50%;
+        left: 50%;
+        min-height: 100%;
+        min-width: 100%;
+        width: auto;
+        transform: translate(-50%, -50%);
+        background-size: cover;
+        z-index: -1;
+
+    }
+</style>
